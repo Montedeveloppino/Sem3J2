0d3bc5b4b0d20d246e151c74b920347bd02f25a7
Labsss <alexandre.labonne@gmail.com>
2019-04-16 18:03:55 +0200

    app3

--- app3.rb
+++ app3.rb
@@ -5,3 +5,16 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 
 require 'pry'
+
+puts "-------------------------------------------------"
+puts "|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
+puts "|Le but du jeu est d'être le dernier survivant !|"
+puts "-------------------------------------------------"
+
+print "Quel est ton nom ? "
+player_name = gets.chomp
+puts "Bonjour #{player_name} !"
+
+my_game = Game.new(player_name)
+
+binding.pry
--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,28 @@
+require_relative 'player'
 
 class Game
-  attr_accessor :human_player :enemies
+  attr_accessor :human_player, :enemies
 
   def initialize(human)
-    @human_player = human
-    @enemies = []
-
-    4.times do 
-    end 
+    @human_player = HumanPlayer.new(human)
+    @enemies = [Player.new("S. Curry"), Player.new("K. Durant"), Player.new("K. Thompson"), Player.new("D. Green")]
   end
 
-  def kill_player(killed)
+  def kill_player
     # Supprime un player de la table enemies
-    @enemies
+    @enemies.each do |gamer|
+      if (gamer.life_points <= 0)
+          @enemies.delete(gamer)
+      end
+    end
   end
-
+ 
   def is_still_ongoing?
+    total = 0
+    @enemies.each do |pv|
+      total += pv.life_points
+    end
+    return true if @human_player.life_points > 0 && total <= 0
     # qui retourne true si le jeu est toujours en cours et false sinon. 
     #Le jeu continue tant que le @human_player a encore des points de vie et qu'il reste des Player à combattre 
     #dans l’array @enemies
@@ -23,26 +30,70 @@ class Game
 
   def show_players
     #va afficher 1) l'état du joueur humain et 2) le nombre de joueurs "bots" restant
+    @human_player.show_state
+    puts "Il reste #{@enemies.count} ennemis en jeu."
+    
   end
 
   def menu
-    #afficher le menu de choix (juste l'afficher, pas plus) 
+    #afficher le menu de choix (juste l'afficher, pas plus)
+    puts "**********************************"
+    puts "Quelle action veux-tu effectuer ? "
+    puts
+    puts "a - chercher une meilleure arme"
+    puts "s - chercher à se soigner "
+    puts
+    puts "attaquer un joueur en vue :"
+    i = 0
+    @enemies.each do |enemie|
+      if enemie.life_points > 0
+        print "#{i} - "
+        enemie.show_state
+        i += 1
+      end
+    end
   end
 
-  def menu_choice
-=begin
-    prend en entrée un string. Cette méthode va permettre de faire réagir le jeu en fonction du choix, dans le menu, de l'utilisateur. 
-    Par exemple, si l'entrée est "a", le @human_player doit aller chercher une arme. Si l'entrée est "0", on le fait attaquer l'ennemi 
-    présenté au choix "0", etc. Pense à faire appel, dans cette méthode, à la méthode kill_player si jamais un Player est tué par 
-    le joueur humain !
-=end
+  def menu_choice()
+    puts
+    puts "Votre choix ?"
+    action = gets.chomp
+    puts "----------------------------------"
+    if action == "a"
+      @human_player.search_weapon
+    end
+    if action == "s"
+      @human_player.search_health_pack
+    end
+    position = action.to_i
+    if @enemies[position].life_points > 0
+      @human_player.attacks(enemies[position])
+      kill_player
+    end
   end
+
 
   def enemies_attack
     #faire riposter tous les ennemis vivants. Ils vont attaquer à tour de rôle le joueur humain
+    puts "----------------------------------" 
+    puts "Les autres joueurs t'attaquent !"
+    @enemies.each do | enemie |
+      if enemie.life_points > 0
+        enemie.attacks(@human_player)
+      end
+    end
   end
 
   def end
     #effectuer l'affichage de fin de jeu. Tu sais, la partie "le jeu est fini" puis "Bravo..." ou "Loser..."
+    puts "**********************************"
+    puts "*** La partie est finie ***"
+    if @human_player.life_points > 0
+      puts "BRAVO ! TU AS GAGNE !"
+    else 
+      puts "Loser ! Tu as perdu !"
+    end
+    puts "**********************************"
   end
+
 end
