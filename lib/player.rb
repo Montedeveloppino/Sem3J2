f477d4171c347cbf839e116d161b2f42cfff0302
notmoebius <notmoebius@gmail.com>
2019-04-16 15:54:42 +0200

    app2 terminé

--- lib/player.rb
+++ lib/player.rb
@@ -46,5 +46,43 @@ class HumanPlayer < Player
   def show_state
     puts "#{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}" 
   end 
+
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end
+
+  def search_weapon
+    weapon_level = rand(1..6) # lancr de dé sera égal au niveau de la nouvelle arme trouvée.
+    puts "Tu as trouvé une arme de niveau #{weapon_level}"
+    if weapon_level > @weapon_level
+      @weapon_level = weapon_level
+      puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+    else
+      puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+    end
+  end
+
+  def search_health_pack
+    pv = rand(1..6)
+    puts pv
+    if pv == 1
+      puts "Tu n'as rien trouvé... " 
+    end
+    if (pv > 1) && (pv < 6)
+      puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+      @life_points = @life_points + 50
+      if @life_points>100
+        @life_points = 100
+      end
+    end
+    if pv == 6
+      puts "Waow, tu as trouvé un pack de +80 points de vie !"
+      @life_points = @life_points + 80
+      if @life_points>100 
+        @life_points = 100 
+      end
+    end
+  end
+
 end
 
